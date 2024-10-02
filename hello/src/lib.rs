c81a69c3a4aa88817220849c6a17a7375abc460e
michrag <31441542+michrag@users.noreply.github.com>
2024-10-02 17:37:23 +0200

    A Worker Struct Responsible for Sending Code from the ThreadPool to a Thread

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,7 +1,20 @@
 use std::thread;
 
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
+    }
+}
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -15,13 +28,13 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
-            // create some threads and store them in the vector
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, f: F)
